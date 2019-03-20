8a1cfcf2d35a8f0c6ba74a5e04352cd6f83fe7c7
Tarun Kumar <tarunvelagala80@gmail.com>
2019-03-20 13:31:46 +0530

    Initial Commit

--- Beginner/evenorodd.py
+++ Beginner/evenorodd.py
@@ -1,8 +1,11 @@
-n = int(input())
-if n < 0:
-    print("invalid")
-else:
-    if n%2 == 0:
-        print("Even")
+try:
+    n = int(input())
+    if n < 0:
+        print("invalid")
     else:
-        print("Odd")
+        if n%2 == 0:
+            print("Even")
+        else:
+            print("Odd")
+except e:
+    print(e)
